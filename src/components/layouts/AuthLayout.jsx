f35f015760815505c2a556c8509178c8a18e5499
elnaz <elnaz@elnazs-MacBook-Pro.local>
2025-10-12 22:59:51 +0330

    Ui fixed.

--- src/components/layouts/AuthLayout.jsx
+++ src/components/layouts/AuthLayout.jsx
@@ -1,12 +1,17 @@
 import React from "react";
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
 import air from "../../assets/img/air.png";
 
 export default function AuthLayout() {
+  const location = useLocation();
   return (
     <div className=" container h-screen m-auto  ">
-      <div className=" h-[450px] relative overflow-hidden">
+      <div className=" h-[450px] relative ">
         <img className="w-full h-full object-cover" src={air} alt="" />
+        <div className="w-full relative bottom-[120px] text-4xl text-white font-bold text-center ">
+          {" "}
+          {location.pathname === "/register" ? "Register" : "Login"}
+        </div>
       </div>
       <div className="p-4 flex flex-col gap-4  ">
         <Outlet />
